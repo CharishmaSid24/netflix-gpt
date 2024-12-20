04b7cc29965a5df588203e66f470939905f9a4ab
CharishmaSid24 <charishmakonjeti@gmail.com>
2024-12-20 12:49:10 +0530

    Login Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,53 @@
+import { useState } from "react";
 import Header from "./Header.js";
 
 const Login = () => {
-    return (
-        <div >
-            <Header />
-            <div className="absolute">
-            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/150c4b42-11f6-4576-a00f-c631308b1e43/web/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_large.jpg" alt="logo" />
-            
-            </div>
-            <form className="relative p-12 bg-black ">
-                <input type="text" placeholder="Email Address" className="p-2 m-2" />
-                <input type="text" placeholder="Password" className="p-2 m-2" />
-                <button className="p-4 m-4">Sign In</button>      
-            </form>
-            
-        </div>
-    )
-}
+  const [isSignInForm, setIsSignInForm] = useState(true);
 
-export default Login; 
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/150c4b42-11f6-4576-a00f-c631308b1e43/web/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-800 rounded-lg"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full bg-gray-800 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-800 rounded-lg"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-2 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix ? Sign Up Now"
+            : "Already Registered ? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
